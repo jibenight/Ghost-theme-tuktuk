4a3e2458783dfbb040f5a1c1dbb6d1b01612e4fb
Jean <nguyen.jean@me.com>
2023-04-27 12:46:18 +0200

    setup font family in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,13 @@
 module.exports = {
   content: ['./**/*.html', './**/*.hbs'],
   darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        nunito: ['nunito', 'arial', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
 };
