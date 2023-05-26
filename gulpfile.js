f200bd98a6384a9c5363973dbadcc05178d90e42
Jean <nguyen.jean@me.com>
2023-05-26 12:29:52 +0200

    update gulp script

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,10 @@ const cssnano = require('cssnano');
 const uglify = require('gulp-uglify');
 const concat = require('gulp-concat');
 const rename = require('gulp-rename');
+const newer = require('gulp-newer');
+
+const { exec } = require('child_process');
+const browserSync = require('browser-sync').create();
 
 // Minifier pour CSS
 function minifyCss() {
@@ -22,10 +26,44 @@ function minifyJs() {
     .pipe(dest('assets/built/js/'));
 }
 
-// Copier les polices vers le dossier build
+// Copier les polices vers le dossier build uniquement si elles ne sont pas déjà présentes
 function copyFonts() {
-  return src('assets/fonts/**').pipe(dest('assets/built/fonts/'));
+  return src('assets/fonts/**')
+    .pipe(newer('assets/built/fonts/'))
+    .pipe(dest('assets/built/fonts/'));
+}
+
+// Tâche pour exécuter la commande postcss
+function runPostcss(cb) {
+  exec(
+    'TAILWIND_MODE=watch postcss assets/css/styles.css -o assets/css/output.css --config tailwind.config.dev.js --watch',
+    function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    }
+  );
+}
+
+// Tâche pour le live reload
+function serve(cb) {
+  browserSync.init({
+    server: {
+      baseDir: './',
+    },
+  });
+
+  // Ajoutez ici les fichiers à surveiller pour le rechargement
+  gulp.watch('assets/css/*.css', series(minifyCss, reload));
+  gulp.watch('assets/css/*.css', series(runPostcss, minifyCss, reload));
+  gulp.watch('**/*.hbs', series(minifyCss, minifyJs, reload));
+}
+
+// Tâche pour recharger le navigateur
+function reload(cb) {
+  browserSync.reload();
+  cb();
 }
 
 // Exporter la tâche par défaut
-exports.build = series(minifyCss, minifyJs, copyFonts);
+exports.run = series(minifyCss, minifyJs, copyFonts, runPostcss, serve);
